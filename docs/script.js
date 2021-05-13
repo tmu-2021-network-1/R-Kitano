1beb79f65d2632bf2da5a9e87f9c48849fe2ba49
RunaKitano <43678464+R-Kitano@users.noreply.github.com>
2021-05-14 00:41:58 +0900

    文字マスクAPIから画像切り替え

--- docs/script.js
+++ docs/script.js
@@ -3,10 +3,30 @@ const id = '1BpGnuwC4lZf9G2yFyiSrxbJuGO8gviV8mr-I2D3x4vA';
 const sheet = 'Studio';
 const endpoint = `${uri}?id=${id}&sheet=${sheet}`;
 
+//let img = new Array("https://industrial-art.sd.tmu.ac.jp/images/studio-images/baba2.png", "https://industrial-art.sd.tmu.ac.jp/images/studio-images/baba1.png", "https://industrial-art.sd.tmu.ac.jp/images/studio-images/ia1.png"); //*1
+let img = [];
+let count = -1; //*2
+
+
+function imgTimer() {
+    //画像番号
+    count++; //*3
+    //画像の枚数確認
+    if (count == img.length) count = 0; //*4
+    //画像出力
+    document.getElementById("mask_img").style.backgroundImage = "url(" + img[count] + ")";
+    //次のタイマー呼びだし
+    setTimeout("imgTimer()", 2000); //*6
+}
+
+
+/*-----------------------------API------------------------------------------------*/
+
 const renderJson = (json) => {
     const studios = json.records;
 
     studios.forEach(studio => {
+        img.push(studio['photo1']);
         const studioDiv = document.createElement('div');
         const studioTitle = document.createElement("span");
         studioTitle.className = 'studio-title';
@@ -35,3 +55,5 @@ const getData = async () => {
 }
 
 getData();
+console.log(img);
+imgTimer();
